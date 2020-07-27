23271b1d3dcdbe6c05bab48a54f58f75a2ccf071
Barbara Gska <pacuszka.barbara@gmail.com>
2020-07-27 14:10:12 +0200

    Add project files

---
+++ FindingEven/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę: ");
+
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine("Podana liczba jest parzysta");
+
+                else
+                    Console.WriteLine("Podana liczba jest nieparzysta");
+            }
+
+            else
+            {
+                Console.WriteLine("Podana wartość jest nieprawidłowa.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
